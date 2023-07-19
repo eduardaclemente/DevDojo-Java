f62b0dae092f83e3fb4f1bef18e54b417c65b2b0
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-07-19 20:16:32 -0300

    operadores aritmeticos - finish

--- src/main/java/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -2,9 +2,10 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula04Operadores {
     public static void main(String[] args) {
+        // + - / *
         int numero1 = 10;
         int numero2 = 20;
-        int resultado = numero1 / numero2;
+        double resultado = numero1 / (double) numero2;
         System.out.println(resultado);
 
 
