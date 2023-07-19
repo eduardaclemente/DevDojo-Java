6313e998297ec8b00dfb3070917f80ee45aaabe0
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-07-19 18:57:46 -0300

    exercicio de tipos

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula03TipoPrimitivoExercicio.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.introducao;
+
+/*
+    Prática:
+
+    Crie variáveis para os campos descritos abaixo entre o <> e imprima a seguinte mensagem:
+
+    Eu <nome>, morando no endereço <endereço>, confirmo que recebi
+    o sala´rio de <salario>, na data <data>
+ */
+public class Aula03TipoPrimitivoExercicio {
+    public static void main(String[] args) {
+
+        String nome = "Eduarda Clemente";
+        String endereco = "Itaquaquecetuba, rua Araras";
+        float salario = 1.500F;
+        String data = "19/07/2023";
+
+
+        System.out.println("Eu " +nome+ ", moando no endereço " +endereco+
+                "confirmo que recebi o salário de " +salario+ ", na data " + data);
+
+
+    }
+}
