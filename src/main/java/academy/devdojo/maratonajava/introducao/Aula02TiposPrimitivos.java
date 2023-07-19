688a3598e1dd41562fe6e321c20445c69d7c5205
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-07-19 17:41:25 -0300

    first commit

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 24; // inicializar a variável
+        System.out.println("A idade é:" + age);
+
+    }
+}
