374ed3fb50039bb0a33ff3ed5a4a1575838fb47c
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-07-20 10:16:22 -0300

    operador ternario DEPOIS

--- src/main/java/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -5,13 +5,9 @@ public class Aula05EstruturasCondicionais03 {
         double salario = 6000;
         String mensagemDoar = "Eu vou doar 500 pro DevDojo!";
         String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter";
-        String resultado; // = salario > 5000 ?  mensagemDoar : mensagemNaoDoar;
+        String resultado = salario > 5000 ?  mensagemDoar : mensagemNaoDoar;
 
-        if (salario > 5000) {
-            resultado = mensagemDoar;
-        } else {
-            resultado = mensagemNaoDoar;
-        }
+
         System.out.println(resultado);
 
     }
