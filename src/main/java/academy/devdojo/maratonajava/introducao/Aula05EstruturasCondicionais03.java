70aaef2641a077382102ba8ef76ae948b076a5e5
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-07-20 10:19:40 -0300

    operador ternario DEPOIS REFATORADO

--- src/main/java/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -3,10 +3,8 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula05EstruturasCondicionais03 {
     public static void main(String[] args) {
         double salario = 6000;
-        String mensagemDoar = "Eu vou doar 500 pro DevDojo!";
-        String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter";
-        String resultado = salario > 5000 ?  mensagemDoar : mensagemNaoDoar;
-
+        boolean possoDoar = salario > 5000;
+        String resultado = possoDoar ?  "Eu vou doar 500 pro DevDojo" : "Ainda não tenho condições, mas vou ter!";
 
         System.out.println(resultado);
 
