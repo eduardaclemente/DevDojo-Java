19f2076bab202d258e34ebb3c1e8d4211f1f9385
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-07-20 09:01:11 -0300

    estrutura de if - finish

--- src/main/java/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -3,10 +3,22 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula05EstruturasCondicionais {
     public static void main(String[] args) {
 
-        int idade = 20;
-        if (false) {
-            System.out.println("Dentro do if");
+        int idade = 15;
+        boolean isAutorizado = idade >=18;
+
+        if (isAutorizado) {
+            System.out.println("Autorizado comprar bebida alcólica");
         }
+
+        if (isAutorizado == false){
+            System.out.println("Não está autorizado a comprar bebida alcólica!");
+        }
+
+        boolean c = true; // estou associando e não comparando
+        if (c == false){
+            System.out.println("Dentro de algo que nunca deve ser feito");
+        }
+
         System.out.println("Fora do if");
 
     }
