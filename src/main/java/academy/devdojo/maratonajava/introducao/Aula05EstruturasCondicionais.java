6ef69435451c294d87fbe992a35eaae815775ab1
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-07-20 08:33:16 -0300

    estrutura de if

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+
+        int idade = 20;
+        if (false) {
+            System.out.println("Dentro do if");
+        }
+        System.out.println("Fora do if");
+
+    }
+}
