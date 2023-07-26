7b3f46d8d04fed7f68d9cbc6172b47b2c4d77d7f
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-07-26 18:59:27 -0300

    exercicio

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula06Exercicio.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06Exercicio {
+    public static void main(String[] args) {
+
+        int count = 0;
+        while (count <= 10000) {
+            System.out.println(count);
+            count +=2;
+        }
+    }
+}
