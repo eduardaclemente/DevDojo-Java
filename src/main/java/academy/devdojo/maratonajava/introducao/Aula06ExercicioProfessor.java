885a3b6d095bc7a7ff8a610d1dca4fe6af11e283
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-07-26 19:43:00 -0300

    exercicio resposta do professor, clean code

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula06ExercicioProfessor.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06ExercicioProfessor {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 1000000000; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
