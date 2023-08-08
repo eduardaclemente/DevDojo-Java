d5b18c112aa8dc19215ff28f18f887410ffe69e6
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-08-08 10:08:30 -0300

    comando break teste1

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula06ExercicioBreak.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06ExercicioBreak {
+    public static void main(String[] args) {
+        double valorTotal = 30000;
+        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela >= 1000){
+                System.out.println("Parcela " + parcela + " R$ " +valorParcela);
+            } else {
+                break;
+            }
+            // System.out.println("Fora do if, mas dentro do for" +parcela);
+        }
+    }
+}
