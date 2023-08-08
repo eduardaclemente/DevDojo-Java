8a387ab8c8ebb81d2ddfc73f3de6ac947d9e6d70
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-08-08 09:18:28 -0300

    exercicio resposta do professor, clean code

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula06Exercicio2.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06Exercicio2 {
+    public static void main(String[] args) {
+
+        int numero = 50;
+        for (int i = numero; i < numero + 25; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
--- src/main/java/academy/devdojo/maratonajava/introducao/Aula06ExercicioProfessor.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula06ExercicioProfessor.java
@@ -3,10 +3,9 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula06ExercicioProfessor {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 1000000000; i++) {
-            if (i % 2 == 0) {
+            int numero = 50;
+            for (int i = numero; i < numero + 25; i++) {
                 System.out.println(i);
             }
         }
     }
-}
